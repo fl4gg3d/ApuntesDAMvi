69781459d8b178016f636713e73c749ce8a6824d
Reuben <fl4gg3d@gmail.com>
2025-09-17 16:02:28 +0200

    Dia 2

---
+++ Goofyass/src/Goofy/hello.java
@@ -0,0 +1,22 @@
+package Goofy;
+
+import java.util.Scanner;
+
+public class hello {
+
+	public static void main(String[] args) {
+		
+		String HelloWorld = "HelloWorld";
+		
+		int respuesta = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		String Hello = sc.nextLine();
+		
+		System.out.println(HelloWorld);
+		
+		sc.close();
+
+	}
+
+}
