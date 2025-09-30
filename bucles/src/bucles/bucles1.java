3aa2f24cbf6ddaafabfd266a227fd8d20420056b
Reuben <fl4gg3d@gmail.com>
2025-09-30 16:53:07 +0200

    Me olvide

---
+++ bucles/src/bucles/bucles1.java
@@ -0,0 +1,16 @@
+package bucles;
+
+public class bucles1 {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		
+		while(a <= 10) {
+			System.out.println("La variable A val:" + a );
+			a++;
+		}
+
+	}
+
+}
