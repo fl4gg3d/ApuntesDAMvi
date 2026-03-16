023359961123d940be9ce47a7312a945ecbcf290
Reuben <fl4gg3d@gmail.com>
2026-03-16 19:33:05 +0100

    fr

---
+++ lenguaje_de_marcas/JavaScript/Funciones 2/1/jsFunciones1.js
@@ -0,0 +1,7 @@
+function abrir(){
+    let window = open("https://www.infojobs.net/candidate/candidate-login/candidate-login.xhtml?dgv=4894552591791534855");
+    setTimeout(()=>{
+        window.close();
+    }, 5000)
+
+}
